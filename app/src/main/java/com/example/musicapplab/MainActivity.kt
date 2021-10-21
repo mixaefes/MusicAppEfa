2961bc8ff1b91dda82f6674ef4cb5dd7e854cec3
mixaefes <mixaefes@gmail.com>
2021-10-21 10:40:35 +0300

    feat: add music service to project

--- app/src/main/java/com/example/musicapplab/MainActivity.kt
+++ app/src/main/java/com/example/musicapplab/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.musicapplab.data.MusicDataUtil
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var glide: RequestManager
